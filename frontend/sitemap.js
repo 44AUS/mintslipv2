9c901026cd4d98c9ade97b4f2012985a3b7e4d59
Austin <austin@supremedetailstudio.com>
2025-12-19 12:02:46 -0500

    added sitemap

---
+++ frontend/sitemap.js
@@ -0,0 +1,78 @@
+export default function sitemap() {
+  const baseUrl = "https://www.mintslip.com";
+
+  return [
+    {
+      url: `${baseUrl}/`,
+      lastModified: new Date(),
+    },
+    {
+      url: `${baseUrl}/self-employed-paystub-generator`,
+      lastModified: new Date(),
+    },
+    {
+      url: `${baseUrl}/contractor-paystub-generator`,
+      lastModified: new Date(),
+    },
+    {
+      url: `${baseUrl}/instant-paystub-generator`,
+      lastModified: new Date(),
+    },
+    {
+      url: `${baseUrl}/accounting-mockup-generator`,
+      lastModified: new Date(),
+    },
+    {
+      url: `${baseUrl}/service-expense-generator`,
+      lastModified: new Date(),
+    },
+    {
+      url: `${baseUrl}/w2-generator`,
+      lastModified: new Date(),
+    },
+    {
+      url: `${baseUrl}/w9-generator`,
+      lastModified: new Date(),
+    },
+    {
+      url: `${baseUrl}/1099-nec-generator`,
+      lastModified: new Date(),
+    },
+    {
+      url: `${baseUrl}/1099-misc-generator`,
+      lastModified: new Date(),
+    },
+    {
+      url: `${baseUrl}/schedule-c-generator`,
+      lastModified: new Date(),
+    },
+    {
+      url: `${baseUrl}/offer-letter-generator`,
+      lastModified: new Date(),
+    },
+    {
+      url: `${baseUrl}/vehicle-bill-of-sale-generator`,
+      lastModified: new Date(),
+    },
+    {
+      url: `${baseUrl}/contact`,
+      lastModified: new Date(),
+    },
+    {
+      url: `${baseUrl}/faq`,
+      lastModified: new Date(),
+    },
+    {
+      url: `${baseUrl}/about`,
+      lastModified: new Date(),
+    },
+    {
+      url: `${baseUrl}/privacy`,
+      lastModified: new Date(),
+    },
+    {
+      url: `${baseUrl}/terms`,
+      lastModified: new Date(),
+    },
+  ];
+}

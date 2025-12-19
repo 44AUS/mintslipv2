cea224dc6b057c51fef3920b0e2577d6703eaf3e
Austin <austin@supremedetailstudio.com>
2025-12-19 12:15:18 -0500

    add xml

--- frontend/next-sitemap.config.js
+++ frontend/next-sitemap.config.js
@@ -1,4 +1,8 @@
+/** @type {import('next-sitemap').IConfig} */
 module.exports = {
   siteUrl: "https://www.mintslip.com",
   generateRobotsTxt: true,
+  sitemapSize: 7000,
+  changefreq: "weekly",
+  priority: 0.7,
 };
